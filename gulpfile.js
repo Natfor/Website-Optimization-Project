483e7de49f5e463085089d7199df059597938880
Natfor <nataforno@gmail.com>
2016-05-25 15:14:37 -0300

    minify portfolio js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 // grab our gulp packages
 var gulp = require('gulp'),
 		cleanCSS = require('gulp-clean-css');
+		uglify = require('gulp-uglify');
 
 //create a default task and just log a message
 gulp.task('default', function(){
@@ -13,4 +14,10 @@ gulp.task('minify-css', function(){
 	return gulp.src('css/*.css')
 		.pipe(cleanCSS())
 		.pipe(gulp.dest('css/build'));
+});
+
+gulp.task('minify-js', function(){
+	return gulp.src('js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('js/build'));
 });
