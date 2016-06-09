8da988c3e506c2cfd4329b9cec80175b2007d3a8
Natfor <nataforno@gmail.com>
2016-06-09 20:33:01 -0300

    analytics code re added

--- js/dev/perfmatters.js
+++ js/dev/perfmatters.js
@@ -19,10 +19,3 @@ window.addEventListener("load", function(event) {
           link.href = "//fonts.googleapis.com/css?family=Open+Sans:400,700";
           document.querySelector("head").appendChild(link);
       })();
-
-/*(function(w,g){w['GoogleAnalyticsObject']=g;
-      w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
-
-      // Optional TODO: replace with your Google Analytics profile ID.
-      ga('create', 'UA-XXXX-Y');
-      ga('send', 'pageview');*/
